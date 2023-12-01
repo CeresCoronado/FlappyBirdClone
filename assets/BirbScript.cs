198a418c44c43b37c2a38dbafab70fe159312a5f
Ceres Coronado <coro.jdan@gmail.com>
2023-11-30 23:27:30 -0700

    Restrucured Birbscript to be more readable

--- assets/BirbScript.cs
+++ assets/BirbScript.cs
@@ -4,10 +4,40 @@ using UnityEngine;
 
 public class BirbScript : MonoBehaviour
 {
-    // Declaring references and variables
+    // Rigidbody2D is a physics object attached to the Bird that carries out this script
     public Rigidbody2D myRigidbody2D;
+    
+    // Variables that can be changed within Unity that effects BirbLift()
     public float flapStrength = 10;
-    public 
+    
+    // Variables that can be changed within Unity that effects BirbTiltUp() and BirbTiltDown()
+    public float initialBirdRotationAngle = 1;
+    public float returnBirdRotationAngle = 1;
+       
+    // BirbLift() Makes the birb go up
+    public void BirbLift()
+    {
+        myRigidbody2D.velocity = Vector2.up * flapStrength;
+    }
+    // BirbTiltUp() Makes the birb tilt its beak facing up
+    public void BirbTiltUp()
+    {
+        float birdRotationVelocityUp = 10;
+        float birdRotationSmoothness = 0;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.z, initialBirdRotationAngle, ref birdRotationVelocityUp, birdRotationSmoothness);
+
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+    // BirbTiltDown() Makes the birb til its beak facing down
+    public void BirbTiltDown()
+    {
+        float birdRotationVelocityDown = 10;
+        float birdRotationSmoothness = 0;
+        float returnAngle = Mathf.SmoothDampAngle(transform.eulerAngles.z, returnBirdRotationAngle, ref birdRotationVelocityDown, birdRotationSmoothness);
+
+        transform.rotation = Quaternion.Euler(0, 0, returnAngle);
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -20,8 +50,19 @@ public class BirbScript : MonoBehaviour
               
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            myRigidbody2D.velocity = Vector2.up * flapStrength;
+            BirbLift();
         }
-        
+         
+    }
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            BirbTiltUp();
+        }
+        else
+        {
+            BirbTiltUp();
+        }
     }
 }
